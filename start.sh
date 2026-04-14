50a4d68193bdbb3d20764c218a4cef2d0535121d
Tsvetanmarkov <tsvetan.markov.a70@learn.telerikacademy.com>
2026-04-14 08:28:13 +0300

    add files for railway deployment

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+export PATH="$PATH:/usr/local/bin:/opt/node/bin:$HOME/.nvm/versions/node/v20.11.0/bin"
+command -v npm >/dev/null 2>&1 || { echo "npm not found, trying to locate..."; }
 cd server
 npm install
 npm start
