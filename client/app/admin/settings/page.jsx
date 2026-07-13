68c9088acd25da54edd3a33a7bc4ba4b60b64ff8
Tsvetanmarkov <tsvetan.markov.a70@learn.telerikacademy.com>
2026-07-13 16:55:20 +0300

    add something

--- client/app/admin/settings/page.jsx
+++ client/app/admin/settings/page.jsx
@@ -839,7 +839,6 @@ export default function SettingsPage() {
                       )}
                     </select>
                   </div>
-                  </div>
                 </div>
                 <div className="flex justify-end gap-3 mt-6">
                   <button
